efad01b180b56086bc1fc713bc517b8ee255567c
arjunjanamatti <arjunjanamatti@gmail.com>
2021-02-11 23:12:02 +0530

    class with video read function

--- read.py
+++ read.py
@@ -22,12 +22,23 @@ import cv2 as cv
 # cv.destroyAllWindows()
 
 class ReadImages:
-    def __init__(self, image):
+    def __init__(self, image, video):
         self.image = image
+        self.video = video
 
     def display_image(self):
         cv.imshow(winname='image_window', mat=self.image)
         cv.waitKey(0)
+
+    def display_videos(self):
+        capture = cv.VideoCapture(self.video)
+        while True:
+            isTrue, frame = capture.read()
+            cv.imshow(winname='play_video', mat=frame)
+            if cv.waitKey(20) & 0xFF == ord('d'):
+                break
+        capture.release()
+        cv.destroyAllWindows()
 
 image = cv.imread(filename = 'C:/Users/Arjun Janamatti/Documents/image_classification/nude_sexy_safe_v1_x320/testing/safe/0AFBAF6E-AC8F-4F86-B131-26AF632BF574.jpg')
 read_image = ReadImages(image)
