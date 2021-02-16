32d02f7d706d08634f21fa4e7f207959b16d954f
arjunjanamatti <arjunjanamatti@gmail.com>
2021-02-16 13:38:31 +0530

    string and int path parameter

--- learn_fast_api_from_documentation/straing_parameter.py
+++ learn_fast_api_from_documentation/straing_parameter.py
@@ -8,5 +8,10 @@ def read_item(item_id):
     return {
         'item_id':item_id
     }
+@api.get(path='/square/{item_id}')
+def read_int_item(item_id:int):
+    return {
+        'square of number':item_id**2
+    }
 
 uvicorn.run(app=api)
