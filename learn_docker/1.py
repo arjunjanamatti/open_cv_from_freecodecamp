a8301aae68e73906979f2cfe9685b49bd856e3d0
arjunjanamatti <arjunjanamatti@gmail.com>
2021-02-26 17:51:58 +0530

    user input option with operation input

--- learn_docker/1.py
+++ learn_docker/1.py
@@ -22,7 +22,14 @@ class calculator:
 a = int(input('Enter the first variable: '))
 b = int(input('Enter the second variable: '))
 calc = calculator(a,b)
-print(f'Addition of {a} and {b} is {calc.add_numbers()}')
-print(f'Multiplication of {a} and {b} is {calc.multiply_numbers()}')
-print(f'Division of {a} and {b} is {calc.devide_numbers()}')
-print(f'Subtraction of {a} and {b} is {calc.subtract_numbers()}')
+operation = input('Enter the opearation between numbers if add, subtract, multiply or devide: ')
+if operation == 'add':
+    print(f'Addition of {a} and {b} is {calc.add_numbers()}')
+elif operation == 'multiply':
+    print(f'Multiplication of {a} and {b} is {calc.multiply_numbers()}')
+elif operation == 'devide':
+    print(f'Division of {a} and {b} is {calc.devide_numbers()}')
+elif operation == 'subtract':
+    print(f'Subtraction of {a} and {b} is {calc.subtract_numbers()}')
+else:
+    print(f'Selected {operation} no option available!!!')
