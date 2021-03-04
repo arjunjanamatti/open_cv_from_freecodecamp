cffdba1ef7b9d32beae46d9b500a409dc8a98dba
arjunjanamatti <arjunjanamatti@gmail.com>
2021-03-04 21:45:54 +0530

    update with dictionary search

--- nlp_feature_extraction/bag_of_words.py
+++ nlp_feature_extraction/bag_of_words.py
@@ -2,4 +2,17 @@ import json
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 
-print(stopwords.words('english'))
+with open('intents.json') as file:
+    data = json.load(file)
+
+print(data)
+
+words_corpus = []
+sentence_list = []
+labels_list = []
+
+
+for dictionary in data['intents']:
+    print(dictionary)
+    print(dictionary['patterns'])
+    print(dictionary['tag'])
