8576f9ad3d0858fafecd883958c59f0251fcaade
arjunjanamatti <arjunjanamatti@gmail.com>
2021-03-05 11:16:13 +0530

    update with comments

--- nlp_feature_extraction/bag_of_words.py
+++ nlp_feature_extraction/bag_of_words.py
@@ -2,7 +2,9 @@ import json
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 from nltk import word_tokenize
+import numpy as np
 
+# load the json file like a dictionary
 with open('intents.json') as file:
     data = json.load(file)
 
@@ -42,24 +44,28 @@ words_corpus = [lemma.lemmatize(words) for words in words_corpus]
 words_corpus = sorted(list(set(words_corpus)))
 print(len(words_corpus))
 
+# here the labels have to be created, hence first will create empty labels and based on the label will fill up as binary bag of words
 labels_empty = [0 for _ in range(len(labels_list))]
 print(labels_empty)
 labels_empty_nested = []
 features_nested = []
 for index, sentence in enumerate(sentence_list):
     features_list = []
-    print(index, sentence)
 
     sentence_lemma = [lemma.lemmatize(words) for words in sentence]
     for words in sentence_lemma:
+        # if word in the sentence in the corpus, add 1 else 0
         if words in words_corpus:
             features_list.append(1)
         else:
             features_list.append(0)
+
+    # now for the labels, based on the label add 1 or 0
     labels_empty_copy = labels_empty[:]
     labels_empty_copy[labels_list.index(sentence_labels_list[index])] = 1
     labels_empty_nested.append(labels_empty_copy)
     features_nested.append(features_list)
-    print(sentence_labels_list[index], labels_list.index(sentence_labels_list[index]))
-print(len(labels_empty_nested))
 
+# convert list to numpy array
+features_nested = np.array(features_nested)
+labels_empty_nested = np.array(labels_empty_nested)
