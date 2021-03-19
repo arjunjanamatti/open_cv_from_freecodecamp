13080795609729867076df3ec0e32eecd3c96879
arjunjanamatti <arjunjanamatti@gmail.com>
2021-03-19 21:42:38 +0530

    simple GCD complete function

--- advanced_python/competitive_programming/gcd_and_lcm.py
+++ advanced_python/competitive_programming/gcd_and_lcm.py
@@ -2,18 +2,16 @@
 number_1 = int(input('Enter the first number: ').strip())
 number_2 = int(input('Enter the second number: ').strip())
 
-if number_1 > number_2:
-    pass
-else:
-    number_1, number_2 = number_2, number_1
-
-for num in range(1, number_1+1):
-    if (number_2 % num == 0) & (number_1 % num == 0):
-        gcd = num
-    else:
-        pass
-
 def simple_gcd(number_1, number_2):
-    pass
+    if number_1 > number_2:
+        pass
+    else:
+        number_1, number_2 = number_2, number_1
 
-print(gcd)
+    for num in range(1, number_1 + 1):
+        if (number_2 % num == 0) & (number_1 % num == 0):
+            gcd = num
+        else:
+            pass
+    return gcd
+
