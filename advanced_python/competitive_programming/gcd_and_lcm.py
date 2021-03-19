aaf893d5becba0129a3766f72a57d0c4aefe10cc
arjunjanamatti <arjunjanamatti@gmail.com>
2021-03-19 22:20:25 +0530

    GCD euclid algorithm

--- advanced_python/competitive_programming/gcd_and_lcm.py
+++ advanced_python/competitive_programming/gcd_and_lcm.py
@@ -11,7 +11,7 @@ def simple_gcd(number_1, number_2):
     else:
         number_1, number_2 = number_2, number_1
 
-    for num in range(1, number_1 + 1):
+    for num in range(1, number_2 + 1):
         if (number_2 % num == 0) & (number_1 % num == 0):
             gcd = num
         else:
@@ -27,7 +27,7 @@ def small_improved_gcd(number_1, number_2):
     else:
         number_1, number_2 = number_2, number_1
 
-    for num in range(1, number_1 - 1):
+    for num in range(1, number_2 - 1):
         if (number_2 % num == 0) & (number_1 % num == 0):
             gcd = num
             break
@@ -37,8 +37,22 @@ def small_improved_gcd(number_1, number_2):
     total_time = round(finish-start, 2)
     return gcd, total_time
 
-gcd_simple, total_time_simple = simple_gcd(number_1, number_2)
-gcd_small_improved, total_time_small_improved = simple_gcd(number_1, number_2)
+def euclid_gcd(number_1, number_2):
+    # The method introduced by Euclid for computing greatest common divisors is based on the fact that, given two positive integers a and b such that a > b, the common divisors of a and b are the same as the common divisors of a – b and b.
+    # So, Euclid's method for computing the greatest common divisor of two positive integers consists of replacing the larger number by the difference of the numbers, and repeating this until the two numbers are equal: that is their greatest common divisor.
+    # This method can be very slow if one number is much larger than the other
+    if number_1 > number_2:
+        pass
+    else:
+        number_1, number_2 = number_2, number_1
+    if number_1 - number_2 == 0:
+        return number_1
+    return euclid_gcd(number_1-number_2, number_2)
 
-print(f'Greatest common devisor of {number_1} and {number_2} using simple method is {gcd_simple} and time taken: {total_time_simple} seconds')
-print(f'Greatest common devisor of {number_1} and {number_2} using simple method is {gcd_small_improved} and time taken: {total_time_small_improved} seconds')
+
+print(euclid_gcd(number_1, number_2))
+# gcd_simple, total_time_simple = simple_gcd(number_1, number_2)
+# gcd_small_improved, total_time_small_improved = simple_gcd(number_1, number_2)
+#
+# print(f'Greatest common devisor of {number_1} and {number_2} using small_improved method is {gcd_small_improved} and time taken: {total_time_small_improved} seconds')
+# print(f'Greatest common devisor of {number_1} and {number_2} using simple method is {gcd_simple} and time taken: {total_time_simple} seconds')
