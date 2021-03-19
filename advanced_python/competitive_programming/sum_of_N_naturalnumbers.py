1ace2d30a1f6ddf11fd5f10ac85bf234e17deb55
arjunjanamatti <arjunjanamatti@gmail.com>
2021-03-19 20:53:34 +0530

    update with both function return

--- advanced_python/competitive_programming/sum_of_N_naturalnumbers.py
+++ advanced_python/competitive_programming/sum_of_N_naturalnumbers.py
@@ -2,10 +2,16 @@
 # simple way
 number = int(input('Enter the number: ').strip())
 sum_numbers = [num for num in range(1,number+1)]
-print(f'sum of first {number} number is {sum(sum_numbers)}')
+
 
 def sum_method_1(number):
-    pass
+    sum_numbers = [num for num in range(1, number + 1)]
+    return sum(sum_numbers)
 
 def sum_method_2(number):
-    pass
+    return number * (number+1)//2
+
+print(number/2)
+print(number//2)
+print(f'sum of first {number} number is {sum_method_1(number)}')
+print(f'sum of first {number} number is {sum_method_2(number)}')
