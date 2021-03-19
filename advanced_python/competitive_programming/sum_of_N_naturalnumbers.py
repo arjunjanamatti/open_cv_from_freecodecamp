0db1e261faa34782763b1e8561e37779f80f5b52
arjunjanamatti <arjunjanamatti@gmail.com>
2021-03-19 20:49:30 +0530

    update

--- advanced_python/competitive_programming/sum_of_N_naturalnumbers.py
+++ advanced_python/competitive_programming/sum_of_N_naturalnumbers.py
@@ -2,4 +2,4 @@
 # simple way
 number = int(input('Enter the number: ').strip())
 sum_numbers = [num for num in range(1,number+1)]
-print(sum(sum_numbers))
+print(f'sum of first {number} number is {sum(sum_numbers)}')
