4c4fcc0c8ee02f22e1955e7f83255eee89af4290
arjunjanamatti <arjunjanamatti@gmail.com>
2021-03-19 20:50:23 +0530

    update with functions

--- advanced_python/competitive_programming/sum_of_N_naturalnumbers.py
+++ advanced_python/competitive_programming/sum_of_N_naturalnumbers.py
@@ -3,3 +3,9 @@
 number = int(input('Enter the number: ').strip())
 sum_numbers = [num for num in range(1,number+1)]
 print(f'sum of first {number} number is {sum(sum_numbers)}')
+
+def sum_method_1(number):
+    pass
+
+def sum_method_2(number):
+    pass
