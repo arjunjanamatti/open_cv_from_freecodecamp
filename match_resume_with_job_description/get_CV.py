992879627d282712e4041d4331b51dc2fc3a18db
arjunjanamatti <arjunjanamatti@gmail.com>
2021-03-21 15:10:42 +0530

    first commit

---
+++ match_resume_with_job_description/get_CV.py
@@ -0,0 +1,30 @@
+from tkinter import Tk
+import os,os.path
+import logging, time
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+PATH = os.path.join(os.getcwd(),'CVs')
+r = Tk()
+r.withdraw()
+
+result = ""
+cvno = 1
+res = ""
+
+try:
+	while (True):
+		result = r.selection_get(selection = 'CLIPBOARD')
+		print(result)
+		if res != result:
+			with open(os.path.join(PATH,'cv'+str(cvno)),'wb') as fout:
+				fout.write(result.encode('utf-8'))
+				res = result
+				logging.info('cv'+str(cvno)+' SUCCESS.')
+				cvno += 1
+				time.sleep(3)
+except Exception as e:
+	selection = None
+# except KeyboardInterrupt:
+# 	r.destroy()
+# 	logging.warning('PGM Interrupted.')
