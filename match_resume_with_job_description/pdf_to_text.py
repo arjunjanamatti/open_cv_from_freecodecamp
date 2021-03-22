02e539c8ab46b30c0536c027233136bbbafb614f
arjunjanamatti <arjunjanamatti@gmail.com>
2021-03-22 12:26:03 +0530

    both multi thread and for loop

--- match_resume_with_job_description/pdf_to_text.py
+++ match_resume_with_job_description/pdf_to_text.py
@@ -2,35 +2,40 @@ import PyPDF2
 import pdfplumber
 import os
 import concurrent.futures
+import time
 
 
 pdf_directory = 'resume_pdf'
 text_directory = 'resume_text'
 
-# for file in os.listdir(f'{pdf_directory}/'):
-#     filename = os.fsdecode(file)
-#     if filename.endswith('.pdf'):
-#         file_name = filename.split('.')[0]
-#         print(filename)
-#         all_text = '' # new line
-#         with pdfplumber.open(pdf_directory+'/'+filename) as pdf:
-#             # page = pdf.pages[0] - comment out or remove line
-#             # text = page.extract_text() - comment out or remove line
-#             for pdf_page in pdf.pages:
-#                single_page_text = pdf_page.extract_text()
-#                # print( single_page_text )
-#                # separate each page's text with newline
-#                all_text = all_text + '\n' + single_page_text
-#             # print(all_text)
-#             with open(f"{text_directory}/{file_name}.txt", "w", encoding="utf-8") as file:
-#                 file.write(all_text)
-#             # print(text) - comment out or remove line
+def ForLoop():
+    start = time.perf_counter()
+    for file in os.listdir(f'{pdf_directory}/'):
+        filename = os.fsdecode(file)
+        if filename.endswith('.pdf'):
+            file_name = filename.split('.')[0]
+            # print(filename)
+            all_text = '' # new line
+            with pdfplumber.open(pdf_directory+'/'+filename) as pdf:
+                # page = pdf.pages[0] - comment out or remove line
+                # text = page.extract_text() - comment out or remove line
+                for pdf_page in pdf.pages:
+                   single_page_text = pdf_page.extract_text()
+                   # print( single_page_text )
+                   # separate each page's text with newline
+                   all_text = all_text + '\n' + single_page_text
+                # print(all_text)
+                with open(f"{text_directory}/{file_name}.txt", "w", encoding="utf-8") as file:
+                    file.write(all_text)
+
+    finish = time.perf_counter()
+    print(f'Total time ForLoop: {round(finish-start, 2)} seconds')
+                # print(text) - comment out or remove line
 
 def pdf_to_text(filename):
-    # filename = os.fsdecode(file)
     if filename.endswith('.pdf'):
         file_name = filename.split('.')[0]
-        print(filename)
+        # print(filename)
         all_text = '' # new line
         with pdfplumber.open(pdf_directory+'/'+filename) as pdf:
             # page = pdf.pages[0] - comment out or remove line
@@ -46,6 +51,14 @@ def pdf_to_text(filename):
 
 # a = [pdf_to_text(os.fsdecode(file)) for file in os.listdir(f'{pdf_directory}/')]
 b = [os.fsdecode(file) for file in os.listdir(f'{pdf_directory}/')]
-print(b)
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    executor.map(pdf_to_text, b)
+
+ForLoop()
+
+def ThreadMultiCheck(b):
+    start = time.perf_counter()
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.map(pdf_to_text, b)
+    finish = time.perf_counter()
+    print(f'Total time ThreadMultiCheck: {round(finish-start, 2)} seconds')
+
+ThreadMultiCheck(b)
