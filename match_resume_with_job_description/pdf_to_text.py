d053b2aca17814a2f92e92805069de0d0bc6802f
arjunjanamatti <arjunjanamatti@gmail.com>
2021-03-22 13:53:20 +0530

    both multi thread and for loop

--- match_resume_with_job_description/pdf_to_text.py
+++ match_resume_with_job_description/pdf_to_text.py
@@ -53,10 +53,10 @@ class ResumeAndJD:
                 with open(f"{text_directory}/{file_name}.txt", "w", encoding="utf-8") as file:
                     file.write(all_text)
 
-    def ThreadMultiCheck(b):
+    def ThreadMultiCheck(self):
         start = time.perf_counter()
         with concurrent.futures.ThreadPoolExecutor() as executor:
-            executor.map(pdf_to_text, b)
+            executor.map(self.PdfToText, self.pdf_list)
         finish = time.perf_counter()
         print(f'Total time ThreadMultiCheck: {round(finish - start, 2)} seconds')
         pass
@@ -64,33 +64,35 @@ class ResumeAndJD:
 
     pass
 
-def pdf_to_text(filename):
-    if filename.endswith('.pdf'):
-        file_name = filename.split('.')[0]
-        # print(filename)
-        all_text = '' # new line
-        with pdfplumber.open(pdf_directory+'/'+filename) as pdf:
-            # page = pdf.pages[0] - comment out or remove line
-            # text = page.extract_text() - comment out or remove line
-            for pdf_page in pdf.pages:
-               single_page_text = pdf_page.extract_text()
-               # print( single_page_text )
-               # separate each page's text with newline
-               all_text = all_text + '\n' + single_page_text
-            # print(all_text)
-            with open(f"{text_directory}/{file_name}.txt", "w", encoding="utf-8") as file:
-                file.write(all_text)
 
 
-
-
-def ThreadMultiCheck(b):
-    start = time.perf_counter()
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        executor.map(pdf_to_text, b)
-    finish = time.perf_counter()
-    print(f'Total time ThreadMultiCheck: {round(finish-start, 2)} seconds')
-
-b = [os.fsdecode(file) for file in os.listdir(f'{pdf_directory}/')]
-
-ThreadMultiCheck(b)
+# def pdf_to_text(filename):
+#     if filename.endswith('.pdf'):
+#         file_name = filename.split('.')[0]
+#         # print(filename)
+#         all_text = '' # new line
+#         with pdfplumber.open(pdf_directory+'/'+filename) as pdf:
+#             # page = pdf.pages[0] - comment out or remove line
+#             # text = page.extract_text() - comment out or remove line
+#             for pdf_page in pdf.pages:
+#                single_page_text = pdf_page.extract_text()
+#                # print( single_page_text )
+#                # separate each page's text with newline
+#                all_text = all_text + '\n' + single_page_text
+#             # print(all_text)
+#             with open(f"{text_directory}/{file_name}.txt", "w", encoding="utf-8") as file:
+#                 file.write(all_text)
+#
+#
+#
+#
+# def ThreadMultiCheck(b):
+#     start = time.perf_counter()
+#     with concurrent.futures.ThreadPoolExecutor() as executor:
+#         executor.map(pdf_to_text, b)
+#     finish = time.perf_counter()
+#     print(f'Total time ThreadMultiCheck: {round(finish-start, 2)} seconds')
+#
+# b = [os.fsdecode(file) for file in os.listdir(f'{pdf_directory}/')]
+#
+# ThreadMultiCheck(b)
