ca62a7459d8105b6d833aa7185c226b2b0a00000
arjunjanamatti <arjunjanamatti@gmail.com>
2021-03-22 14:04:35 +0530

    both multi thread and for loop

--- match_resume_with_job_description/pdf_to_text.py
+++ match_resume_with_job_description/pdf_to_text.py
@@ -5,7 +5,9 @@ import time
 
 
 pdf_directory = 'resume_pdf'
-text_directory = 'resume_text'
+resume_text_directory = 'resume_text'
+jd_text_directory = 'jd_text'
+job_description = 'job_description.pdf'
 
 # def ForLoop():
 #     start = time.perf_counter()
@@ -32,8 +34,11 @@ text_directory = 'resume_text'
 #                 # print(text) - comment out or remove line
 
 class ResumeAndJD:
-    def __init__(self, pdf_directory):
+    def __init__(self, pdf_directory, resume_text_directory, job_description, jd_text_directory):
         self.pdf_directory = pdf_directory
+        self.resume_text_directory = resume_text_directory
+        self.job_description = job_description
+        self.jd_text_directory = jd_text_directory
         self.pdf_list = [os.fsdecode(file) for file in os.listdir(f'{self.pdf_directory}/')]
 
     def PdfToText(self, filename):
@@ -50,8 +55,27 @@ class ResumeAndJD:
                     # separate each page's text with newline
                     all_text = all_text + '\n' + single_page_text
                 # print(all_text)
-                with open(f"{text_directory}/{file_name}.txt", "w", encoding="utf-8") as file:
+                with open(f"{self.resume_text_directory}/{file_name}.txt", "w", encoding="utf-8") as file:
                     file.write(all_text)
+
+    def JdPdftoText(self):
+        filename = self.job_description
+        if filename.endswith('.pdf'):
+            file_name = filename.split('.')[0]
+            # print(filename)
+            all_text = ''  # new line
+            with pdfplumber.open(pdf_directory + '/' + filename) as pdf:
+                # page = pdf.pages[0] - comment out or remove line
+                # text = page.extract_text() - comment out or remove line
+                for pdf_page in pdf.pages:
+                    single_page_text = pdf_page.extract_text()
+                    # print( single_page_text )
+                    # separate each page's text with newline
+                    all_text = all_text + '\n' + single_page_text
+                # print(all_text)
+                with open(f"{file_name}.txt", "w", encoding="utf-8") as file:
+                    file.write(all_text)
+
 
     def ThreadMultiCheck(self):
         start = time.perf_counter()
@@ -61,10 +85,13 @@ class ResumeAndJD:
         print(f'Total time ThreadMultiCheck: {round(finish - start, 2)} seconds')
         pass
 
+        pass
+
 
     pass
 
-
+new_instance = ResumeAndJD(pdf_directory, resume_text_directory)
+new_instance.ThreadMultiCheck()
 
 # def pdf_to_text(filename):
 #     if filename.endswith('.pdf'):
