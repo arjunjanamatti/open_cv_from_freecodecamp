546baadf0a204470a5abf69101ffd88c63a4207e
arjunjanamatti <arjunjanamatti@gmail.com>
2021-03-23 15:51:59 +0530

    method to train the model

--- match_resume_with_job_description/NER_entities/resume_summarization.py
+++ match_resume_with_job_description/NER_entities/resume_summarization.py
@@ -30,3 +30,41 @@ for ann in annotation_list:
     for ent in ann['entities']:
         print(ent[0], ent[2])
 
+nlp = spacy.blank('en')
+
+def train_model(text_list, annotation_list):
+    if 'ner' not in nlp.pipe_names:
+        ner = nlp.create_pipe(factory_name='ner')
+        nlp.add_pipe(factory_name='ner', last=True)
+
+    for annotation in annotation_list:
+        for ent in annotation['entities']:
+            ner.add_label(ent[2])
+
+    other_pipes = [pipe for pipe in nlp.pipe_names if pipe!='ner']
+    with nlp.disable_pipe(*other_pipes):
+        optimizer = nlp.begin_training()
+        for itn in range(10):
+            print(f'Start Iteration: {itn}')
+            random.shuffle(annotation_list)
+            random.shuffle(text_list)
+            losses = {}
+            index = 0
+            for text, annotation in text_list, annotation_list:
+                try:
+                    nlp.update(
+                        [text],
+                        [annotation],
+                        drop=0.2,
+                        sgd=optimizer,
+                        losses=losses
+                    )
+                except Exception as e:
+                    print(f'Exception: {e}')
+
+            print(losses)
+
+
+
+train_model(text_list, annotation_list)
+nlp.to_disk('nlp_model')
