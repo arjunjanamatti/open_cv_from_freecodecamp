ad40381ad839040b226dde2452fa4af549bd1531
arjunjanamatti <arjunjanamatti@gmail.com>
2021-03-23 12:44:41 +0530

    pickle the data

--- match_resume_with_job_description/NER_entities/resume_summarization.py
+++ match_resume_with_job_description/NER_entities/resume_summarization.py
@@ -1,6 +1,7 @@
 import spacy
 import pickle
 import random
+import pandas as pd
 
 # with open('train_data.txt', 'r', encoding='utf-8') as file:
 #     train_data = file.read()
@@ -9,7 +10,22 @@ import random
 # with open(file='train_data.pkl', mode = 'wb') as file:
 #     pickle.dump(train_data, file)
 
-with open('train_data.pkl', 'rb', encoding='utf-8') as file:
-    train_data = file.read()
+try:
+    with open(file='train_data.pickle', mode='rb') as file:
+        word_list_df = pickle.load(file)
+except:
+    # df = pd.read_csv('train_data.txt', header=None)
+    # word_list_df = [words for words in df.iloc[:, -1]]
 
-print(train_data)
+    with open('train_data.txt', 'r', encoding='utf-8') as file:
+        word_list_df = file.read()
+
+    with open('train_data.pickle', mode='wb') as file:
+        pickle.dump(word_list_df, file)
+
+print(word_list_df)
+
+# with open('train_data.pkl', 'rb', encoding='utf-8') as file:
+#     train_data = file.read()
+#
+# print(train_data)
