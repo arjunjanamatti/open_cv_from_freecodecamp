62d59f38c858e2a39cec80f299ef51b8371da3b3
arjunjanamatti <arjunjanamatti@gmail.com>
2021-03-23 20:03:42 +0530

    third commit use the custom entities model

--- match_resume_with_job_description/NER_entities/use_entities_custom_model.py
+++ match_resume_with_job_description/NER_entities/use_entities_custom_model.py
@@ -1,3 +1,21 @@
 import spacy
+import sys, fitz
 
-nlp = spacy.load('nlp_model')
+nlp_model = spacy.load('nlp_model')
+
+doc = fitz.open('damoder.pdf')
+text = ''
+for page in doc:
+    text = text + str(page.getText())
+
+text_join = "".join(text.split('\n'))
+# print(text_join)
+doc = nlp_model(text_join)
+print(doc)
+print()
+print(doc.ents)
+print(nlp_model)
+# for ent in doc.ents:
+#     print(ent)
+    # print(f'{ent.label_.upper():{30}} - {ent.text}')
+
