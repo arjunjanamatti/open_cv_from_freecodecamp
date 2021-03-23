edd3bc2e81d45110c1da5bb50f0fa1a8372c0fe6
arjunjanamatti <arjunjanamatti@gmail.com>
2021-03-23 15:15:41 +0530

    pickle the data with method

--- match_resume_with_job_description/NER_entities/resume_summarization.py
+++ match_resume_with_job_description/NER_entities/resume_summarization.py
@@ -4,23 +4,24 @@ import random
 import pandas as pd
 from ast import literal_eval
 
-# df = pd.read_csv('train_data.txt', sep='\n', header=None)
-#
-# df.to_pickle('train_data.pickle')
+def GetEntities(text_file):
+    try:
+        df_1 = pd.read_pickle(f'{text_file}.pickle')
+    except Exception as e:
+        df = pd.read_csv(f'{text_file}.txt', sep='\n', header=None)
+        df.to_pickle(f'{text_file}.pickle')
+        df_1 = pd.read_pickle(f'{text_file}.pickle')
 
-# content_list = [_ for _ in df.iloc[:,-1]]
-# with open('train_data.pickle', 'wb') as file:
-#     pickle.dump(content_list,file)
 
-df_1 = pd.read_pickle('train_data.pickle')
+    content_list = [_ for _ in df_1.iloc[:, -1]]
+    new_updated_list = []
+    for j in range(len(content_list)):
+        for i in ((literal_eval(content_list[j]))):
+            new_updated_list.append(i)
 
-content_list = [_ for _ in df_1.iloc[:,-1]]
-print(len(content_list[0]))
-print(((literal_eval(content_list[0])))[0])
-print(((literal_eval(content_list[0])))[1])
-new_updated_list = []
-for j in range(len(content_list)):
-    for i in  ((literal_eval(content_list[j]))):
-        new_updated_list.append(i)
+    new_updated_list_1 = new_updated_list[1::2]
+    return new_updated_list_1
 
-print(new_updated_list[1])
+text_file = 'train_data'
+new_updated_list_1 = GetEntities(text_file)
+print(new_updated_list_1)
