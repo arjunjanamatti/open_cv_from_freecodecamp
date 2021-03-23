4349c6147b84d3e0c7d73bfb655ce1d72c6e30db
arjunjanamatti <arjunjanamatti@gmail.com>
2021-03-23 16:09:14 +0530

    method to train the model

--- match_resume_with_job_description/NER_entities/resume_summarization.py
+++ match_resume_with_job_description/NER_entities/resume_summarization.py
@@ -20,29 +20,29 @@ def GetEntities(text_file):
             new_updated_list.append(i)
 
     annotation_list = new_updated_list[1::2]
-    text_list = new_updated_list[1::2]
-    return text_list, annotation_list
+    text_list = new_updated_list[0::2]
+    return text_list, annotation_list, new_updated_list
 
 text_file = 'train_data'
-text_list, annotation_list = GetEntities(text_file)
+text_list, annotation_list, new_updated_list = GetEntities(text_file)
 
-for ann in annotation_list:
-    for ent in ann['entities']:
-        print(ent[0], ent[2])
+# for ann in annotation_list:
+#     for ent in ann['entities']:
+#         print(ent[0], ent[2])
 
 nlp = spacy.blank('en')
 
 def train_model(text_list, annotation_list):
     if 'ner' not in nlp.pipe_names:
-        ner = nlp.create_pipe(factory_name='ner')
-        nlp.add_pipe(factory_name='ner', last=True)
+        ner = nlp.create_pipe('ner')
+        nlp.add_pipe(ner, last=True)
 
     for annotation in annotation_list:
         for ent in annotation['entities']:
             ner.add_label(ent[2])
 
     other_pipes = [pipe for pipe in nlp.pipe_names if pipe!='ner']
-    with nlp.disable_pipe(*other_pipes):
+    with nlp.disable_pipes(*other_pipes):
         optimizer = nlp.begin_training()
         for itn in range(10):
             print(f'Start Iteration: {itn}')
@@ -50,7 +50,7 @@ def train_model(text_list, annotation_list):
             random.shuffle(text_list)
             losses = {}
             index = 0
-            for text, annotation in text_list, annotation_list:
+            for text, annotation in zip(text_list, annotation_list):
                 try:
                     nlp.update(
                         [text],
