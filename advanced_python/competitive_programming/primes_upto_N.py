58cae56a08aa6b8ddade05702d57a42102a00c63
arjunjanamatti <arjunjanamatti@gmail.com>
2021-03-31 22:24:11 +0530

    prime numbers upto N with better approach

--- advanced_python/competitive_programming/primes_upto_N.py
+++ advanced_python/competitive_programming/primes_upto_N.py
@@ -24,10 +24,26 @@ def check_prime(number):
         return False
 
 def prime_upto_N(number):
+    # check if every number from 1 to number is a prime
     num_list = []
     for num in range(1, number):
         if check_prime(num):
             num_list.append(num)
     return num_list
 
+def gen_primes(number):
+    primes  = [True]*(number+1)
+    primes[0] = False
+    primes[1] = False
+    for p in range(2, int((number**0.5)+1)):
+        if primes[p] == True:
+            for i in range(p*p, number+1, p):
+                primes[i] = False
+
+    prime_list = []
+    for i in range(0,len(primes)):
+        if primes[i] == True:
+            prime_list.append(i)
+    return prime_list
 print(f'Prime Numbers till {number} is {prime_upto_N(number)}')
+print(f'Prime Numbers till {number} is {gen_primes(number)}')
