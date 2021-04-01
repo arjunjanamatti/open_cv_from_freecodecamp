d82bb1562ec88a5b46834d91eafeb84b4cf636eb
arjunjanamatti <arjunjanamatti@gmail.com>
2021-04-01 19:29:41 +0530

    min_max_sum

---
+++ advanced_python/HackerRank/min_max_sum.py
@@ -0,0 +1,16 @@
+# Given 5 positive integers, find minimum and maximum values that can be calculated by summing
+# exactly four of the five integers. Then print respective minimum and maximum values as a
+# single line of two space-seperated long integers
+
+# array = [1,3,5,7,9], minimum_sum = 1+3+5+7 = 16, maximum_sum = 3+5+7+9 = 24
+arr = list(map(int, input().rstrip().split()))
+
+def min_and_max(arr):
+    arr = sorted(arr)
+    min_sum = sum(arr[:-1])
+    max_sum = sum(arr[1:])
+    print(min_sum, end=' ')
+    print(max_sum)
+    return min_sum, max_sum
+
+min_sum, max_sum = min_and_max(arr)
