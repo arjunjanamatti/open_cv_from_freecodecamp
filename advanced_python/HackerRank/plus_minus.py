b4700d52ccb944c4ca96b6c8c5653c18ee78fb0b
arjunjanamatti <arjunjanamatti@gmail.com>
2021-04-01 21:22:29 +0530

    decimal of positive, negative and zero

--- advanced_python/HackerRank/plus_minus.py
+++ advanced_python/HackerRank/plus_minus.py
@@ -6,7 +6,18 @@ def plusMinus(arr):
     pos_num = 0
     neg_num = 0
     zero_num = 0
-    pass
+    for i in arr:
+        if i > 0:
+            pos_num += 1
+        elif i < 0:
+            neg_num += 1
+        else:
+            zero_num += 1
+    print(pos_num/len(arr))
+    print(neg_num/len(arr))
+    print(zero_num/len(arr))
+
+    return pos_num/len(arr), neg_num/len(arr), zero_num/len(arr)
 
 if __name__ == '__main__':
     n = int(input())
