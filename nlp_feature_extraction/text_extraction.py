c0ad8b655f53b606c5ad65d5288f38fe1294837e
arjunjanamatti <arjunjanamatti@gmail.com>
2021-05-29 22:39:32 +0530

    convert to dataframe from dates

--- nlp_feature_extraction/text_extraction.py
+++ nlp_feature_extraction/text_extraction.py
@@ -15,6 +15,7 @@ print(data)
 data = [d.lower() for d in data]
 
 import re
+import pandas as pd
 
 data_2019_birthday, data_2020_birthday = [], []
 for only in data:
@@ -35,5 +36,22 @@ print(data_2020_birthday)
 
 date, time, user_id, message = [], [], [], []
 
-print(f'date: {(data_2020_birthday[0].split("-")[0]).split(",")[0]}, time: {(data_2020_birthday[0].split("-")[0]).split(",")[-1]}, '
-      f'user_id: {(data_2020_birthday[0].split("-")[-1]).split(":")[0]}, message: {(data_2020_birthday[0].split("-")[-1]).split(":")[-1]}' )
+def get_data(partial_data):
+    date.append((partial_data.split("-")[0]).split(",")[0])
+    time.append((partial_data.split("-")[0]).split(",")[-1])
+    user_id.append((partial_data.split("-")[-1]).split(":")[0])
+    message.append((partial_data.split("-")[-1]).split(":")[-1])
+    # return date, time, user_id, message
+
+
+# print(list(map(get_data, data_2020_birthday)))
+list(map(get_data, data_2020_birthday))
+#
+raw_df_2020 = pd.DataFrame()
+raw_df_2020['date']  = date
+raw_df_2020['time']  = time
+raw_df_2020['user_id']  = user_id
+raw_df_2020['message']  = message
+print(raw_df_2020)
+raw_df_2020.to_csv('raw_df_2020.csv')
+
