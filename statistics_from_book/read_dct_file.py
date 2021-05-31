f2c28b90501d965eae2b9889f37fae7c6fdd20c2
arjunjanamatti <arjunjanamatti@gmail.com>
2021-05-31 11:49:42 +0530

    calculated prglength

--- statistics_from_book/read_dct_file.py
+++ statistics_from_book/read_dct_file.py
@@ -6,14 +6,38 @@ start_time = time.perf_counter()
 stata_dict = parse_stata_dict(file="2002FemPreg.dct")
 data = pd.read_fwf("2002FemPreg.dat", names=stata_dict.names, colspecs=stata_dict.colspecs)
 
+
+
+##### Selecting only required columns from 243 available columns
+# caseid is the integer ID of the respondent
+# prglength is the integer duration of pregnancy in weeks
+# outcome is an integer code for the outcome of pregnancy. Code 1 indicates a live birth
+# pregordr is pregnancy serial number; for first pregnancy it is 1, and second pregnancy is 2 and so on
+# birthord is serial number for live births; code for first child is 1, and so on. For outcomes other than live birth, this field is blank
+# birthwgt_lb and birthwgt_oz contains the pounds and ounces parts of the birth weight of the baby
+# agepreg is the mother's age at the end of pregnancy
+# finalwgt is the statistical weight assosciated with the respondent. It is the floating point value that indicates the number of people in US population this respondent represents.
+
+
+# selected_variables_data = data[['caseid', 'prglength', 'outcome', 'pregordr',
+#                                 'birthord', 'birthwgt_lb', 'birthwgt_oz',
+#                                 'agepreg', 'finalwgt']]
+
+data['prglength'] = (data.apply(lambda x:x['wksgest'] if x['wksgest'] else x['mosgest']*4.33, axis=1))
+
+selected_variables_data = data[['caseid', 'prglength', 'outcome', 'pregordr',
+                                'birthord', 'birthwgt_lb', 'birthwgt_oz',
+                                'agepreg', 'finalwgt']]
+
+print(selected_variables_data.info())
+
+print()
+
+print(selected_variables_data['agepreg'])
+
 end_time = time.perf_counter()
 total_time = end_time - start_time
 print(f'Total time: {total_time}')
-print()
-print(data.head())
-print()
-print(f'Shape of the dataframe: {data.shape}')
-print(len(data.columns))
 
 
 
