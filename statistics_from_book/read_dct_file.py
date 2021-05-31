8577bc3d0ed1341a93be7875cbb0f2e305f32fc1
arjunjanamatti <arjunjanamatti@gmail.com>
2021-05-31 10:38:30 +0530

    added time count for the script

--- statistics_from_book/read_dct_file.py
+++ statistics_from_book/read_dct_file.py
@@ -1,13 +1,21 @@
 import pandas as pd
 from statadict import parse_stata_dict
+import time
 
+start_time = time.perf_counter()
 stata_dict = parse_stata_dict(file="2002FemPreg.dct")
 data = pd.read_fwf("2002FemPreg.dat", names=stata_dict.names, colspecs=stata_dict.colspecs)
 
+end_time = time.perf_counter()
+total_time = end_time - start_time
+print(f'Total time: {total_time}')
+print()
 print(data.head())
 print()
 print(f'Shape of the dataframe: {data.shape}')
-print(data.columns)
+print(len(data.columns))
+
+
 
 
 
